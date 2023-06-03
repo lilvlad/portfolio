da003ffdd71b174c44410078bfbbb4951df93bf4
lilvlad <vladsamodelkin1994@gmail.com>
2023-06-04 00:21:23 +0100

    main

--- js/tilt.js
+++ js/tilt.js
@@ -1,55 +1,39 @@
 VanillaTilt.init(document.querySelector("#image-comparison-slider"), {
-  // Tilt Effect - vanilla-tilt.js (https://micku7zu.github.io/vanilla-tilt.js/) is required for this
-  max: 5, // max tilt rotation (degrees (deg))
-  speed: 700, // speed (transition-duration) of the enter/exit transition (milliseconds (ms))
-  scale: 1.05, // transform scale - 2 = 200%, 1.5 = 150%, etc..
+  max: 5,
+  speed: 700,
+  scale: 1.05,
 });
-
-const slider = document.querySelector("#image-comparison-slider");
-const sliderImgWrapper = document.querySelector(
-  "#image-comparison-slider .img-wrapper"
-);
-const sliderHandle = document.querySelector("#image-comparison-slider .handle");
-
-slider.addEventListener("mousemove", sliderMouseMove, { passive: true });
-slider.addEventListener("touchmove", sliderMouseMove, { passive: true });
-
-function sliderMouseMove(event) {
+const slider = document.querySelector("#image-comparison-slider"),
+  sliderImgWrapper = document.querySelector(
+    "#image-comparison-slider .img-wrapper"
+  ),
+  sliderHandle = document.querySelector("#image-comparison-slider .handle");
+function sliderMouseMove(e) {
   if (isSliderLocked) return;
-
-  const sliderLeftX = slider.offsetLeft;
-  const sliderWidth = slider.clientWidth;
-  const sliderHandleWidth = sliderHandle.clientWidth;
-
-  let mouseX = (event.clientX || event.touches[0].clientX) - sliderLeftX;
-  if (mouseX < 0) mouseX = 0;
-  else if (mouseX > sliderWidth) mouseX = sliderWidth;
-
-  sliderImgWrapper.style.width = `${((1 - mouseX / sliderWidth) * 100).toFixed(
-    4
-  )}%`;
-  sliderHandle.style.left = `calc(${((mouseX / sliderWidth) * 100).toFixed(
-    4
-  )}% - ${sliderHandleWidth / 2}px)`;
+  let s = slider.offsetLeft,
+    i = slider.clientWidth,
+    d = sliderHandle.clientWidth,
+    r = (e.clientX || e.touches[0].clientX) - s;
+  r < 0 ? (r = 0) : r > i && (r = i),
+    (sliderImgWrapper.style.width = `${((1 - r / i) * 100).toFixed(4)}%`),
+    (sliderHandle.style.left = `calc(${((r / i) * 100).toFixed(4)}% - ${
+      d / 2
+    }px)`);
 }
-
-let isSliderLocked = false;
-
-slider.addEventListener("mousedown", sliderMouseDown, { passive: true });
-slider.addEventListener("touchstart", sliderMouseDown, { passive: true });
-slider.addEventListener("mouseup", sliderMouseUp, { passive: true });
-slider.addEventListener("touchend", sliderMouseUp, { passive: true });
-slider.addEventListener("mouseleave", sliderMouseLeave, { passive: true });
-
-function sliderMouseDown(event) {
-  if (isSliderLocked) isSliderLocked = false;
-  sliderMouseMove(event);
+slider.addEventListener("mousemove", sliderMouseMove, { passive: !0 }),
+  slider.addEventListener("touchmove", sliderMouseMove, { passive: !0 });
+let isSliderLocked = !1;
+function sliderMouseDown(e) {
+  isSliderLocked && (isSliderLocked = !1), sliderMouseMove(e);
 }
-
 function sliderMouseUp() {
-  if (!isSliderLocked) isSliderLocked = true;
+  isSliderLocked || (isSliderLocked = !0);
 }
-
 function sliderMouseLeave() {
-  if (isSliderLocked) isSliderLocked = false;
+  isSliderLocked && (isSliderLocked = !1);
 }
+slider.addEventListener("mousedown", sliderMouseDown, { passive: !0 }),
+  slider.addEventListener("touchstart", sliderMouseDown, { passive: !0 }),
+  slider.addEventListener("mouseup", sliderMouseUp, { passive: !0 }),
+  slider.addEventListener("touchend", sliderMouseUp, { passive: !0 }),
+  slider.addEventListener("mouseleave", sliderMouseLeave, { passive: !0 });
